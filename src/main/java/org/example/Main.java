c438d2f35d0a36c2554cbd0c27fd2158a83b05a2
swadi <kswadi01@gmail.com>
2024-02-25 19:43:35 +0530

    added logging changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static double addition(double x, double y){
         double result = x+y;
@@ -10,9 +15,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
+
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome to Scientific Calculator Application!");
+        logger.info("Hello and welcome to Scientific Calculator Application!");
 
         //TODO: Functions
     }
